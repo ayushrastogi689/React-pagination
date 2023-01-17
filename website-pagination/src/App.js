01fb7b13db7e1134f21dd20c8a917b3506255358
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-17 18:06:44 +0000

    Created a asynchronous function and used fetch to get all the dummy products

--- website-pagination/src/App.js
+++ website-pagination/src/App.js
@@ -1,7 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
+import {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+  // Here fetchProduct is an arrow function which is asynchronous in nature
+  const fetchProduct = async() => {
+
+    const [product, setProduct] = useState([]);
+
+    // After providing the URL this becomes a asynchronous call, hence we need to provide await
+    const response = await fetch("https://dummyjson.com/products?limit=100");
+
+  }
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
       
