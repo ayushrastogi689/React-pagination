86e303bdadf34294b00fc5fe8bdce47e1ff05aaa
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-17 18:15:48 +0000

    Added a const data to convert the response in json format

--- website-pagination/src/App.js
+++ website-pagination/src/App.js
@@ -8,8 +8,14 @@ function App() {
 
     const [product, setProduct] = useState([]);
 
-    // After providing the URL this becomes a asynchronous call, hence we need to provide await
+    // We can use fetch() to get all the products from api.
+       // After providing the URL in fetch() it becomes a asynchronous call, hence we need to provide await to resolve the promise 
     const response = await fetch("https://dummyjson.com/products?limit=100");
+    
+    // Converting the response into json format.
+       // We put await to wait for the further operation  
+    const data = await response.json();
+    
 
   }
 
